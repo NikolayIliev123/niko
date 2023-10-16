377e08295da264d8d963f715032d3b4ce28dc647
ogledalna <ogledalna@OGLEDALNA-RM03>
2023-10-16 15:41:27 +0300

    Add project files.

---
+++ niko/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace niko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
